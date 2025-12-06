b67c2d117f11e28a5a1eaf776add7a7c2eda8d59
vijayalakshmi421 <vijayalakshmirasamala@gmail.com>
2025-12-06 14:34:07 +0530

    Create ArrayDemo.java

---
+++ ArrayDemo.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
