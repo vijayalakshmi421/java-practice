cf6d7a70df66821b5eff820a8f8c10329b3d89c9
vijayalakshmi421 <vijayalakshmirasamala@gmail.com>
2025-12-06 14:49:12 +0530

    Create MyLinkedList.java

---
+++ MyLinkedList.java
@@ -0,0 +1,30 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class MyLinkedList {
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        head.next = second;
+        second.next = third;
+
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+
+        System.out.println("null");
+    }
+}
