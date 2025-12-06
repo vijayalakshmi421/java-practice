86aeba6edd2a123d41001b9fe23604dcbe1c5667
vijayalakshmi421 <vijayalakshmirasamala@gmail.com>
2025-12-06 14:35:09 +0530

    Create MethodDemo.java

---
+++ MethodDemo.java
@@ -0,0 +1,10 @@
+public class MethodDemo {
+
+    public static void greet() {
+        System.out.println("Hello Vijaya, welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
