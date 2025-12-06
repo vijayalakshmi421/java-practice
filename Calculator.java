121b4dba4517ff687f3134d7684bb0a6cb0dc0a5
vijayalakshmi421 <vijayalakshmirasamala@gmail.com>
2025-12-06 14:41:30 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Choose operation (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        int result = 0;
+
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+
+            case '-':
+                result = a - b;
+                break;
+
+            case '*':
+                result = a * b;
+                break;
+
+            case '/':
+                result = a / b;
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println("Result = " + result);
+    }
+}
